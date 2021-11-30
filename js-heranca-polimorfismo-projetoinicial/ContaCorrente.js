c2a8363f649e4c5ae2e924a04c6b0aed5388a331
wilberchair <wilberchair@gmail.com>
2021-11-30 18:38:17 -0300

    iniciando modulo 2 e criacao do arquivo contapoupanca

--- js-heranca-polimorfismo-projetoinicial/ContaCorrente.js
+++ js-heranca-polimorfismo-projetoinicial/ContaCorrente.js
@@ -2,12 +2,7 @@ import { Cliente } from "./Cliente.js";
 
 export class ContaCorrente{
     static numeroDeContas = 0;
-    agencia;
-    _cliente;
-   // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
-   _saldo = 0;
-    
-
+   // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields    
 
     set cliente(novoValor){
         if(novoValor instanceof Cliente){
@@ -26,6 +21,7 @@ export class ContaCorrente{
     constructor(agencia, cliente){
         this.agencia = agencia;
         this.cliente = cliente;
+        this._saldo = 0;
         ContaCorrente.numeroDeContas += 1;
     }
 
