969f9b12774f51bb2afd92156adfc87d8e34da3c
wilberchair <wilberchair@gmail.com>
2022-02-13 22:45:23 -0300

    chore(autenticacao): criando o metodo para verificar autenticacao

--- js-heranca-polimorfismo-projetoinicial/SistemaAutenticacao.js
+++ js-heranca-polimorfismo-projetoinicial/SistemaAutenticacao.js
@@ -1,5 +1,14 @@
+// Ser autenticável significa ter o método autenticar
+
 export class SistemaAutenticacao {
-  static login(funcionario, senha) {
-    return funcionario.senha == senha
+  static login(autenticavel, senha) {
+    if(SistemaAutenticacao.ehAutenticavel(autenticavel)){
+      return autenticavel.autenticar(senha);
+    }
+    return false;
+  }
+
+  static ehAutenticavel(autenticavel) {
+    return 'autenticar' in autenticavel && autenticavel.autenticar instanceof Function;
   }
 }
--- js-heranca-polimorfismo-projetoinicial/index.js
+++ js-heranca-polimorfismo-projetoinicial/index.js
@@ -6,7 +6,11 @@ import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 const diretor = new Diretor('Rodrigo', 10000, 12345678900);
 diretor.cadastrarSenha('123456')
 const gerente = new Gerente('Ricardo', 5000, 12345678901);
+gerente.cadastrarSenha('123')
 
-const estaLogado = SistemaAutenticacao.login(diretor, "123456");
+const cliente = new Cliente('Laís', 78945612399, '456')
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, "123456");
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "123");
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, "456");
 
-console.log(estaLogado)
+console.log(diretorEstaLogado, gerenteEstaLogado, clienteEstaLogado)
