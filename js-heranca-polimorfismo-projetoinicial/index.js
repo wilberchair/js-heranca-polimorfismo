2bc61fada59000d678a89ff1584a3e46adac7128
wilberchair <wilberchair@gmail.com>
2022-02-13 22:00:43 -0300

    chore(conta): criando as instancias de diretor e gerente

--- js-heranca-polimorfismo-projetoinicial/index.js
+++ js-heranca-polimorfismo-projetoinicial/index.js
@@ -1,13 +1,8 @@
 import {Cliente} from "./Cliente.js"
-import { ContaCorrente } from "./Conta/ContaCorrente.js";
-import { ContaPoupanca } from "./Conta/ContaPoupanca.js";
-import { ContaSalario } from "./Conta/ContaSalario.js";
+import { Gerente } from './Funcionario/Gerente.js'
+import { Diretor } from './Funcionario/Diretor.js'
 
-const cliente1 = new Cliente("Ricardo", 11122233309);
-const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
-const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
-const contaSalario = new ContaSalario(cliente1)
-contaSalario.depositar(100)
-contaSalario.sacar(10)
+const diretor = new Diretor('Rodrigo', 10000, 12345678900);
+const gerente = new Gerente('Ricardo', 5000, 12345678901);
 
-console.log(contaSalario)
+SistemaAutenticacao.login(diretor, "123456789")
