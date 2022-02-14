1346c6cb8442361a0ff0b7fc29df789f83912ba2
wilberchair <wilberchair@gmail.com>
2022-02-13 22:10:51 -0300

    chore(autenticacao): criacao da senha de autenticacao do diretor

--- js-heranca-polimorfismo-projetoinicial/index.js
+++ js-heranca-polimorfismo-projetoinicial/index.js
@@ -4,8 +4,9 @@ import { Diretor } from './Funcionario/Diretor.js'
 import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 
 const diretor = new Diretor('Rodrigo', 10000, 12345678900);
+diretor.cadastrarSenha('123456')
 const gerente = new Gerente('Ricardo', 5000, 12345678901);
 
-const estaLogado = SistemaAutenticacao.login(diretor, "123456789");
+const estaLogado = SistemaAutenticacao.login(diretor, "123456");
 
 console.log(estaLogado)
